58bec0dbf9158af77e49743c872767d95d163bb3
SuhailaXF <suhailafaizarohma@gmail.com>
2025-05-20 08:33:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("romakelapa")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "rawrma"
 )
 st.image("IMG-20250519-WA0030.jpg")
 st.write("so cuteeetyy")
