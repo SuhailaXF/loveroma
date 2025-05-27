e243079d0b7d085c9e8a9d6c340f07caf727d0fc
SuhailaXF <suhailafaizarohma@gmail.com>
2025-05-27 07:53:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,10 @@ st.write(
 )
 st.image("IMG-20250519-WA0030.jpg")
 st.write("so cuteeetyy")
+st.title("aplikasi sederhana")
+st.header("aplikasi mengecek nilai genap/ganjil")
+angka = st.number_input("tulis sebuah angka:", value=0, step=1)
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah bilangan genap")
+else:
+    st.write(f"{angka} adalah bilangan bulat")
